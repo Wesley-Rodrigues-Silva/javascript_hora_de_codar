05ebf4097fcc5a381381f9dc75190de4c62b3f53
Wesley Rodrigues Silva <161177795+Wesley-Rodrigues-Silva@users.noreply.github.com>
2024-06-12 20:29:46 -0300

    att

---
+++ 26_SWITCH/js/script.js
@@ -0,0 +1,15 @@
+const job = "Advogado"
+
+switch(job) {
+    case 'Programador':
+        console.log('Você é um programador!')
+        break
+    case 'Advogado':
+    console.log('Você é um advogado!')
+        break
+    case 'Advogado':
+        console.log('Você é um engenheiro!')
+        break;
+    default:
+        console.log('Profissão não encontrada')
+}
