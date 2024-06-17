92162e42327342d2c15b87f4031d290ee8488e2f
Wesley Rodrigues Silva <161177795+Wesley-Rodrigues-Silva@users.noreply.github.com>
2024-06-17 18:49:43 -0300

    Att

---
+++ 35_METODOS/js/script.js
@@ -0,0 +1,15 @@
+const numbers = [5, 3, 4];
+
+const otherNumbers = [1, 2, 3];
+
+const allNumbers = numbers.concat(otherNumbers);
+
+console.log(allNumbers);
+
+const text = 'algum texto';
+
+console.log(text.toUpperCase()); // deixar todas as letras maiuscula
+
+console.log(typeof text.toUpperCase()) // retorna que é uma function
+
+console.log(text.indexOf('g')) // localizar o indice da letra g
